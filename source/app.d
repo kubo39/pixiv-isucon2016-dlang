bbc8222af16772dddfd74dd0de496d7208bef9cb
Hiroki Noda <kubo39@gmail.com>
2017-06-19 05:53:02 +0900

    Switch to mysql-native (#3)
    Goodbye mysql-lited!

--- source/app.d
+++ source/app.d
@@ -5,7 +5,7 @@ import vibe.http.session;
 import vibe.templ.diet;
 import vibe.http.fileserver;
 
-import mysql; // mysql-lited
+import mysql;
 
 import std.algorithm;
 import std.conv : to;
@@ -13,43 +13,44 @@ import std.datetime;
 import std.regex;
 import std.stdio;
 import std.uuid;
-
+import std.variant;
 
 immutable POST_PER_PAGE = 20;
 
-MySQLClient client;
+MySQLPool client;
 
 struct User
 {
-    uint id;
+    int id;
     string account_name;
     string passhash;
     bool authority;
     bool del_flg;
-    Date created_at;
+    DateTime created_at;
 }
 
 struct Post
 {
-    uint id;
-    uint user_id;
-    string text;  // `body` is reserved keyword in D.
-    Date created_at;
+    int id;
+    int user_id;
+    string body_;
     string mime;
-    uint comment_count;
+    DateTime created_at;
+    long comment_count;
     User user;
     Comment[] comments;
 }
 
 struct Comment
 {
-    uint id;
-    uint post_id;
-    uint user_id;
+    int id;
+    int post_id;
+    int user_id;
     string comment;
-    Date created_at;
+    DateTime created_at;
     User user;
 }
+
 
 /**
  * Utils
@@ -62,13 +63,12 @@ void dbInitialize()
         "delete from users where id > 1000",
         "delete from posts where id > 10000",
         "delete from comments where id > 10000",
-        // "update users set del_flg = 0",
-        // "update users set del_flg = 1 where id % 50 = 0"
+        "update users set del_flg = 0",
+        "update users set del_flg = 1 where id % 50 = 0"
         ];
-    foreach (q; sqls)
-    {
+    foreach (q; sqls) {
         writeln(q);
-        conn.execute(q);
+        conn.exec(q);
     }
 }
 
@@ -77,7 +77,7 @@ string digest(string src)
     import std.algorithm : findSplitAfter;
     import std.process : executeShell, escapeShellCommand;
     import std.string : strip;
-    return executeShell(escapeShellCommand("print", `"`,  src, `"`, "| openssl dgst -sha512"))
+    return executeShell(`echo "` ~ escapeShellCommand(src) ~ `"| openssl dgst -sha512`)
         .output
         .findSplitAfter("= ")[1]  // opensslのバージョンによっては (stdin)= というのがつくので取る
         .strip;
@@ -96,10 +96,18 @@ string calculatePasshash(string accountName, string password)
 User tryLogin(string accountName, string password)
 {
     auto conn = client.lockConnection();
-    User user;
-    conn.execute("select * from users where account_name = ? and del_flg = 0", accountName, (MySQLRow row) {
-            user = row.toStruct!User;
-        });
+    auto select = conn.prepare("select * from users where account_name = ? and del_flg = 0");
+    select.setArgs(accountName);
+    ResultRange range = select.query();
+    Row row = range.front;
+    auto user = User(
+                     row[0].get!(int),
+                     row[1].get!(string),
+                     row[2].get!(string),
+                     row[3].get!(byte).to!bool,
+                     row[4].get!(byte).to!bool,
+                     row[5].get!(DateTime)
+                     );
     if (calculatePasshash(user.account_name, password) == user.passhash)
         return user;
     return User.init;
@@ -109,15 +117,21 @@ User getSessionUser(HTTPServerRequest req, HTTPServerResponse res)
 {
     if (!req.session)
         return User.init;
-    if (req.session.isKeySet("user"))
-    {
+    if (req.session.isKeySet("user")) {
         auto conn = client.lockConnection();
-        User user;
         auto id = req.session.get("user", "id");
-        conn.execute("select * from users where id = ?", id, (MySQLRow row) {
-                user = row.toStruct!User;
-            });
-        return user;
+        auto select = conn.prepare("select * from users where id = ?");
+        select.setArgs(id);
+        auto range = select.query();
+        auto row = range.front;
+        return User(
+                    row[0].get!(int),
+                    row[1].get!(string),
+                    row[2].get!(string),
+                    row[3].get!(byte).to!bool,
+                    row[4].get!(byte).to!bool,
+                    row[5].get!(DateTime)
+                    );
     }
     return User.init;
 }
@@ -157,37 +171,70 @@ Post[] makePosts(Post[] results, bool allComments=false)
     Post[] posts;
     posts.reserve(POST_PER_PAGE);
 
-    foreach (post; results)
-    {
-        conn.execute("select count(*) as count from comments where post_id = ?", post.id, (MySQLRow row) {
-                struct DummyCount
-                {
-                    uint count;
-                }
-                post.comment_count = row.toStruct!DummyCount.count;
-            });
+    foreach (post; results) {
+        {
+            auto select = conn.prepare("select count(*) as count from comments where post_id = ?");
+            select.setArgs(post.id);
+            auto range = select.query();
+            auto row = range.front;
+            post.comment_count = row[0].get!(long);
+        }
 
-        auto query = "select * from comments where post_id = ? order by created_at desc";
+        auto queryStmt = "select * from comments where post_id = ? order by created_at desc";
         if (!allComments)
-            query ~= " limit 3";
+            queryStmt ~= " limit 3";
 
         Comment[] comments;
-        conn.execute(query, post.id, (MySQLRow row) {
-                comments ~= row.toStruct!(Comment, Strict.no);
-            });
 
-        foreach (comment; comments)
         {
-            conn.execute("select * from users where id = ?", comment.user_id, (MySQLRow row) {
-                    comment.user = row.toStruct!User;
-                });
+            auto select = conn.prepare(queryStmt);
+            select.setArgs(post.id);
+            auto range = select.query();
+            foreach (row; range) {
+                comments ~= Comment(
+                                    row[0].get!(int),
+                                    row[1].get!(int),
+                                    row[2].get!(int),
+                                    row[3].get!(string),
+                                    row[4].get!(DateTime),
+                                    );
+            }
         }
+
+        {
+            foreach (c; comments) {
+                auto select = conn.prepare("select * from users where id = ?");
+                select.setArgs(c.user_id);
+                auto range = select.query();
+                auto row = range.front;
+                c.user = User(
+                              row[0].get!(int),
+                              row[1].get!(string),
+                              row[2].get!(string),
+                              row[3].get!(byte).to!bool,
+                              row[4].get!(byte).to!bool,
+                              row[5].get!(DateTime)
+                              );
+            }
+        }
+
         reverse(comments);
         post.comments = comments;
 
-        conn.execute("select * from users where id = ?", post.user_id, (MySQLRow row) {
-                post.user = row.toStruct!User;
-            });
+        {
+            auto select = conn.prepare("select * from users where id = ?");
+            select.setArgs(post.user_id);
+            auto range = select.query();
+            auto row = range.front;
+            post.user =User(
+                            row[0].get!(int),
+                            row[1].get!(string),
+                            row[2].get!(string),
+                            row[3].get!(byte).to!bool,
+                            row[4].get!(byte).to!bool,
+                            row[5].get!(DateTime)
+                            );
+        }
 
         if (!post.user.del_flg)
             posts ~= post;
@@ -203,19 +250,24 @@ Post[] makePosts(Post[] results, bool allComments=false)
  * Handler.
  */
 
-
 void getIndex(HTTPServerRequest req, HTTPServerResponse res)
 {
-    if (getSessionUser(req, res) !is User.init)
-    {
+    if (getSessionUser(req, res) !is User.init) {
         auto conn = client.lockConnection();
         auto csrf_token = req.session.get("csrf_token", "");
         Post[] posts;
         posts.reserve(POST_PER_PAGE);
 
-        conn.execute("select id, user_id, text, created_at, mime from posts order by created_at desc limit 20", (MySQLRow row) {
-                posts ~= row.toStruct!(Post, Strict.no);
-            });
+        auto range = conn.query("select id, user_id, body, mime, created_at from posts order by created_at desc limit 20");
+        foreach (row; range) {
+            posts ~= Post(
+                          row[0].get!(int),
+                          row[1].get!(int),
+                          row[2].get!(string),
+                          row[3].get!(string),
+                          row[4].get!(DateTime),
+                          );
+        }
         posts = makePosts(posts);
         return res.render!("index.dt", posts, csrf_token);
     }
@@ -226,15 +278,13 @@ void getIndex(HTTPServerRequest req, HTTPServerResponse res)
 void postIndex(HTTPServerRequest req, HTTPServerResponse res)
 {
     auto me = getSessionUser(req, res);
-    if (me is User.init)
-    {
-        writeln("postIndex() no login.");
+    if (me is User.init) {
+        stderr.writeln("postIndex() no login.");
         return res.redirect("/login");
     }
 
-    if (req.form["csrf_token"] != req.session.get("csrf_token", ""))
-    {
-        writeln("トークンが違います");
+    if (req.form["csrf_token"] != req.session.get("csrf_token", "")) {
+        stderr.writeln("トークンが違います");
         enforceHTTP(false, HTTPStatus.unprocessableEntity, httpStatusText(HTTPStatus.unprocessableEntity));
     }
     return res.redirect("/");
@@ -255,8 +305,7 @@ void postLogin(HTTPServerRequest req, HTTPServerResponse res)
         return res.redirect("/");
 
     auto user = tryLogin(req.form["account_name"], req.form["password"]);
-    if (user !is User.init)
-    {
+    if (user !is User.init) {
         if (!req.session)
             req.session = res.startSession();
         req.session.set("user", user.id.to!string);
@@ -288,22 +337,21 @@ void postRegister(HTTPServerRequest req, HTTPServerResponse res)
         return res.redirect("/register");
 
     auto conn = client.lockConnection();
-    bool isSet = false;
-    conn.execute("select 1 from users where account_name = ?", accountName, (MySQLRow row) {
-            isSet = true;
-        });
-    if (isSet)
-    {
-        writeln("アカウント名がすでに使われています");
+    auto select = conn.prepare("select 1 from users where account_name = ?");
+    select.setArgs(accountName);
+    auto row = select.query();
+    if (row.count != 0) {
+        stderr.writeln("アカウント名がすでに使われています");
         return res.redirect("/register");
     }
 
-    conn.execute("insert into users (account_name, passhash) values (?, ?)",
-                 accountName, calculatePasshash(accountName, password));
+    auto insert = conn.prepare("insert into users (account_name, passhash) values (?, ?)");
+    insert.setArgs(accountName, calculatePasshash(accountName, password));
+    insert.exec();
 
     if (!req.session)
         req.session = res.startSession();
-    req.session.set("user", conn.insertID.to!string);
+    req.session.set("user", conn.lastInsertID.to!string);
     req.session.set("csrf_token", randomUUID().to!string);
     return res.redirect("/");
 }
@@ -324,13 +372,19 @@ void getPosts(HTTPServerRequest req, HTTPServerResponse res)
     Post[] posts;
     posts.reserve(POST_PER_PAGE);
 
-    conn.execute("select id, user_id, text, mime, created_at from posts order by created_at desc limit 20", (MySQLRow row) {
-            posts ~= row.toStruct!(Post, Strict.no);
-        });
+    auto range = conn.query("select id, user_id, body, mime, created_at from posts order by created_at desc limit 20");
+    Post post;
+    foreach (row; range) {
+        posts ~= Post(
+                      row[0].get!(int),
+                      row[1].get!(int),
+                      row[2].get!(string),
+                      row[3].get!(string),
+                      row[4].get!(DateTime),
+                      );
+    }
     posts = makePosts(posts);
-
     string csrf_token = "";
-
     return res.render!("posts.dt", posts, csrf_token);
 }
 
@@ -341,9 +395,18 @@ void getPostsId(HTTPServerRequest req, HTTPServerResponse res)
     Post[] posts;
     posts.reserve(POST_PER_PAGE);
 
-    conn.execute("select * from posts where id = ?", req.params["id"], (MySQLRow row) {
-            posts ~= row.toStruct!(Post, Strict.no);
-        });
+    auto select = conn.prepare("select id, user_id, body, mime, created_at from posts where id = ?");  // Do not use `*` !
+    select.setArgs(req.params["id"]);
+    auto range = select.query();
+    foreach (row; range) {
+        posts ~= Post(
+                      row[0].get!(int),
+                      row[1].get!(int),
+                      row[2].get!(string),
+                      row[3].get!(string),
+                      row[4].get!(DateTime),
+                      );
+    }
     posts = makePosts(posts, true);  // assign `allComments` = true.
     if (!posts.length)
         enforceHTTP(false, HTTPStatus.notFound, httpStatusText(HTTPStatus.notFound));
@@ -359,45 +422,73 @@ void getPostsId(HTTPServerRequest req, HTTPServerResponse res)
 void getUserList(HTTPServerRequest req, HTTPServerResponse res)
 {
     auto conn = client.lockConnection();
+
     User user;
-    conn.execute("select * from users where account_name = ? and del_flg = 0", req.params["account_name"], (MySQLRow row) {
-            user = row.toStruct!User;
-        });
+    {
+        auto select = conn.prepare("select * from users where account_name = ? and del_flg = 0");
+        select.setArgs(req.params["account_name"]);
+        auto range = select.query();
+        auto row = range.front;
+        user = User(
+                    row[0].get!(int),
+                    row[1].get!(string),
+                    row[2].get!(string),
+                    row[3].get!(byte).to!bool,
+                    row[4].get!(byte).to!bool,
+                    row[5].get!(DateTime)
+                    );
+    }
     if (user is User.init)
         return res.writeBody("", 404);
 
+
     Post[] posts;
     posts.reserve(POST_PER_PAGE);
-    conn.execute("select id, user_id, text, mime, created_at from posts where user_id = ? order by created_at desc", user.id, (MySQLRow row) {
-            posts ~= row.toStruct!(Post, Strict.no);
-        });
+
+    {
+        auto select = conn.prepare("select id, user_id, body, mime, created_at from posts where user_id = ? order by created_at desc");
+        select.setArgs(user.id);
+        auto range = select.query();
+        foreach (row; range) {
+            posts ~= Post(
+                          row[0].get!(int),
+                          row[1].get!(int),
+                          row[2].get!(string),
+                          row[3].get!(string),
+                          row[4].get!(DateTime),
+                          );
+        }
+    }
+
     posts = makePosts(posts);
 
-    uint commentCount;
-    conn.execute("select count(*) as count from comments where user_id = ?", user.id, (MySQLRow row) {
-            struct DummyCount
-            {
-                uint count;
-            }
-            commentCount = row.toStruct!DummyCount.count;
-        });
+    int commentCount;
+    {
+        auto select = conn.prepare("select count(*) as count from comments where user_id = ?");
+        select.setArgs(user.id);
+        auto range = select.query();
+        auto row = range.front;
+        commentCount = row[0].get!(int);
+    }
 
     uint[] postIds;
-    conn.execute("select id from posts where user_id = ?", user.id, (MySQLRow row) {
-            postIds ~= row.toStruct!(Post, Strict.no).id;
-        });
+    {
+        auto select = conn.prepare("select id from posts where user_id = ?");
+        select.setArgs(user.id);
+        auto range = select.query();
+        foreach (row; range) {
+            postIds ~= row[0].get!(int);
+        }
+    }
     size_t postCount = postIds.length;
 
-    uint commentedCount;
-    if (postCount)
-    {
-        conn.execute("select count(*) as count from comments where post_id in ?", postIds, (MySQLRow row) {
-                struct DummyCount
-                {
-                    uint count;
-                }
-                commentedCount = row.toStruct!DummyCount.count;
-            });
+    int commentedCount;
+    if (postCount) {
+        auto select = conn.prepare("select count(*) as count from comments where post_id in ?");
+        select.setArgs(postIds);
+        auto range = select.query();
+        auto row = range.front;
+        commentedCount = row[0].get!(int);
     }
 
     auto me = getSessionUser(req, res);
@@ -416,7 +507,7 @@ version(unittest) {}
 else
 shared static this()
 {
-    client = new MySQLClient("host=localhost;port=3306;user=root;pwd=password;db=isuconp");
+    client = new MySQLPool("host=localhost;port=3306;user=root;pwd=password;db=isuconp");
 
     auto router = new URLRouter;
     router.get("/", &getIndex);
